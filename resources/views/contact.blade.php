f43d356522ad3762bcc97db128038e53d0914d77
mohihsanfaozan <mohihsanfz@gmail.com>
2021-03-14 20:39:43 +0700

    ihsan

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+@section('content')
+
+<h3>Halaman contact</h3>
+
+@stop
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+@section('content')
+
+<h3>WELCOME Website</h3>
+<h5>NIKE</h3>
+Berbasis dari nama dewi Yunani yang berarti kemenangan, Nike didirikan tahun 1964 ketika atlet sekaligus pengusaha Oregon bernama Phillip Knight, mengagas impor sepatu lari dari Jepang untuk bersaing dengan merek Jerman seperti Adidas dan Puma yang kemudian mendominasi pasar Amerika Serikat. Keuntungannya adalah bahwa sepatu Jepang lebih murah karena tenaga kerja lebih murah di Jepang.
+
+@stop
